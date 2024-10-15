cda9bd89f030a327f64a4475a23ade4c932d0896
Hugo Ramon <hramonp-dev@outlook.com>
2024-10-15 20:39:20 -0300

    Added cookie consent with CookieBot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { useState } from "react";
+import ReactCookieBot from "react-cookiebot";
+
+const domainGroupId = "0ae05c41-14dd-4099-b440-d0bb13199788";
+
 export default function Home() {
+  const [hasCookieBot, setHasCookieBot] = useState<boolean>(false);
+
   return (
     <div>
       <h1>Next.js Boilerplate</h1>
+      <Button
+        onClick={() => setHasCookieBot(!!document.querySelector("#CookieBot"))}
+      >
+        Click here to check if CookieBot is running in this website
+      </Button>
+
+      <p>{hasCookieBot && "CookieBot is running in this website!"}</p>
+      <ReactCookieBot domainGroupId={domainGroupId} />
     </div>
   );
 }
