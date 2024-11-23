b386aa6458762b7e2d8a151101c1928838940fdf
Hugo Ramon <hramonp-dev@outlook.com>
2024-11-22 23:19:53 -0300

    Upgraded to Next.js 15, React.js 19

---
+++ app/layout.tsx
@@ -0,0 +1,35 @@
+import type { Metadata } from "next";
+import localFont from "next/font/local";
+import "./globals.css";
+
+const geistSans = localFont({
+  src: "./fonts/GeistVF.woff",
+  variable: "--font-geist-sans",
+  weight: "100 900",
+});
+const geistMono = localFont({
+  src: "./fonts/GeistMonoVF.woff",
+  variable: "--font-geist-mono",
+  weight: "100 900",
+});
+
+export const metadata: Metadata = {
+  title: "Next | Shadcn Template",
+  description: "A template with Next.js 15, React.js 19 and Shadcn-UI",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
---
+++ components/form-multi-step.tsx
@@ -0,0 +1,3 @@
+export default function FormMultiStep() {
+  return <div>Multistep Form</div>;
+}
